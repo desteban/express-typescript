5e13957a2d77f9aed7e376245b7fe93258a1b219
desteban <esteban28c@hotmail.com>
2020-11-03 14:35:33 -0500

    middlewares para express

--- src/app.ts
+++ src/app.ts
@@ -15,17 +15,19 @@ export class app {
   }
 
   settings() {
+    // definir el puerto
     this.app.set("port", this.port || process.env.PORT || 3000);
   }
 
   middlewares() {
     this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    // datos de formulario
+    this.app.use(express.urlencoded({ extended: false }));
   }
 
   routes() {
     this.app.use(IndexRoutes);
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
   }
 
   async listen() {
---
+++ src/keys.sample.ts
@@ -0,0 +1,8 @@
+export const keys = {
+  host: "",
+  user: "",
+  database: "",
+  connectionLimit: 0,
+  port: 0,
+  password: "",
+};
